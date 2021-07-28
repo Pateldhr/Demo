59ad35ebc4bec90ad43ad98fc664d59578eae185
Pateldhr <dhruvi@silverwingteam.com>
2021-07-28 19:11:19 +0530

    initial upload

--- app/src/main/java/com/example/api_php_demo/model/Variablebag.java
+++ app/src/main/java/com/example/api_php_demo/model/Variablebag.java
@@ -2,5 +2,5 @@ package com.example.api_php_demo.model;
 
 public class Variablebag {
 
-    public static final String BaseUrl = "http://localhost:8080/mobile/";
+    public static final String BaseUrl = "http://192.168.64.2/mobile/";
 }
